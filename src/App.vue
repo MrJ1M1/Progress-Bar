4886c4e76e33fb3f67455a19301edbad85755f69
MrJ1M1 <razzaqovjamshidbek1@gmail.com>
2022-10-29 12:15:32 +0500

    I have made project name of 'progress-bar'

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,72 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <main>
+      <div class="navigation">
+        <header>
+          <h1>Jamshidbek Razzaqov</h1>
+        </header>
+        <nav>
+          <a href="#">About us</a>
+          <a href="#">Contact</a>
+          <a href="#">Portfolio</a>
+        </nav>
+      </div>
+      <progressbar :progress="progress"/>
+      <post @progressUpdate="setProgress"/>
+    </main>
+  </div>
 </template>
 
+<script>
+import Post from './components/Post.vue'
+import Progressbar from './components/Progressbar.vue'
+export default {
+  components: { Post, Progressbar },
+  data() {
+    return {
+      progress: 0
+    }
+  },  
+  methods: {
+    setProgress(progress){
+      this.progress = progress;
+    }
+  },
+}
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body{
+    font-family: sans-serif;
+  }
+  main{
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    overflow: hidden;
+  }
+  .navigation{
+    width: 100%;
+    height: 10vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #002337;
+    padding: 30px;
+  }
+  header h1{
+    margin-left: 10px;
+    font-size: 32px;
+    color: aliceblue;
+  }
+  nav a{
+    margin: 0 20px;
+    text-decoration: none;
+    color: #fff;
+    text-transform: uppercase;
+  }
 </style>
---
+++ src/components/Progressbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="progress-bar"> 
+        <div :class="(progress === 100) ? 'progress-inner complete' : 'progress-inner'" :style="`width: ${progress}%`">
+            <span :class="(progress > 50) ? 'white' : ''">{{progress}}%</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'progrssbar',
+    props: ['progress']
+}
+</script>
+<style scoped>
+    .progress-bar{
+        width: 100%;
+        height: 20px;
+        background: #fff;
+        position: relative;
+    }
+    .progress-inner{
+        height: 100%;
+        background: linear-gradient(to right, #ED008D, #2484E4, #002337);
+        border-radius: 0 10px 10px 0;
+    }
+    .progress-inner span{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: #313131;
+        font-size: 14px;
+    }
+    .progress-inner.complete{
+        border-radius: 0;
+    }
+    .progress-inner span.white{
+        color: #fff;
+    }
+</style>
